a1b20b237e59118fa223d0a7aa1d4eb818c1091c
Li.Guan <guanli_developer@aliyun.com>
2023-01-21 17:39:58 +0800

    remove unnecessary modifier

--- src/main/java/tech/guanli/boot/internal/model/constant/ResponseDataConstant.java
+++ src/main/java/tech/guanli/boot/internal/model/constant/ResponseDataConstant.java
@@ -2,12 +2,12 @@ package tech.guanli.boot.internal.model.constant;
 
 public interface ResponseDataConstant {
 
-	static final int SUCCESS = 0;
+	int SUCCESS = 0;
 
-	static final int FAILED = 1;
+	int FAILED = 1;
 
-	static final String SUCCESS_MESSAGE = "success";
+	String SUCCESS_MESSAGE = "success";
 
-	static final String FAILED_MESSAGE = "failed";
+	String FAILED_MESSAGE = "failed";
 
 }
